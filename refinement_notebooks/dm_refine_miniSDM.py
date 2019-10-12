80cc3e902e0bb82809bbd1f10aa60e0916b04be2
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2019-10-11 23:41:05 -0600

    made devicenum an input parameter and search sigma can be float

--- refinement_notebooks/dm_refine_miniSDM.py
+++ refinement_notebooks/dm_refine_miniSDM.py
@@ -12,7 +12,7 @@ logger = logging.getLogger('rfpipe')
 from refine_utils import cd_refine    
 import argparse
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
 
 params = {
     'axes.labelsize' : 14,
@@ -26,8 +26,9 @@ params = {
 matplotlib.rcParams.update(params)
 
 def refine(miniSDMlist, workdir, preffileloc, gainpath = '/home/mchammer/evladata/telcal/', 
-           search_sigma = 7, refine = True, classify = True, dm = 350, dm_frac=0.2, dm_steps = 100):
+           search_sigma = 7, refine = True, classify = True, dm = 350, dm_frac=0.2, dm_steps = 100, devicenum = '0'):
 
+    os.environ['CUDA_VISIBLE_DEVICES'] = devicenum
     # Searching for gainfile
     gainfile = []
     for sdm in miniSDMlist:
@@ -86,11 +87,12 @@ if __name__ == "__main__":
     parser.add_argument('-dmf', '--DM_fraction', help='Fraction of DM around central DM to search',
                         required=False, type=float, default=0.3)
     parser.add_argument('-dms', '--DM_steps', help='Number of DM steps', required=False, type=int, default=200)
-    parser.add_argument('-s', '--search_sigma', help='Sigma of the image for search', required=False, type=int, default=7)
+    parser.add_argument('-s', '--search_sigma', help='Sigma of the image for search', required=False, type=float, default=7)
     parser.add_argument('-r', '--refinement_plots', help='Generate and Save refinement plots', action='store_true')
     parser.add_argument('-c', '--classify', help='Classify the candidates using FETCH', action='store_true')
+    parser.add_argument('-d', '--devicenum', help='Enter the GPU number to run pipeline', type=str, default=0, required=False)
     args = parser.parse_args()
     
     refine(miniSDMlist=glob.glob(args.sdm_path), workdir=args.workdir, preffileloc=args.preffileloc, 
            gainpath=args.gainpath, search_sigma = args.search_sigma, refine = args.refinement_plots,
-           classify = args.classify, dm = args.DM, dm_frac=args.DM_fraction, dm_steps = args.DM_steps)
+           classify = args.classify, dm = args.DM, dm_frac=args.DM_fraction, dm_steps = args.DM_steps, devicenum=args.devicenum)
