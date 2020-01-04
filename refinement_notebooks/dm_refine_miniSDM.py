7a9c7bbbdbe3488fa84ea7838fad53bc0d719387
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-01-03 22:34:43 -0700

    fixed logging and inputs

--- refinement_notebooks/dm_refine_miniSDM.py
+++ refinement_notebooks/dm_refine_miniSDM.py
@@ -26,7 +26,7 @@ params = {
 matplotlib.rcParams.update(params)
 
 def refine(miniSDMlist, workdir, preffileloc, gainpath = '/home/mchammer/evladata/telcal/', 
-           search_sigma = 7, refine = True, classify = True, dm = 350, dm_frac=0.2, dm_steps = 100, devicenum = '0'):
+           search_sigma = 7, refine = True, classify = True, dm = 350, dm_frac=0.2, dm_steps = 100, devicenum = '0', band='l'):
 
     os.environ['CUDA_VISIBLE_DEVICES'] = devicenum
     # Searching for gainfile
@@ -36,7 +36,7 @@ def refine(miniSDMlist, workdir, preffileloc, gainpath = '/home/mchammer/evladat
         datasetId = '{0}'.format('_'.join(sdmname.split('_')[1:-1]))
         # # set the paths to the gainfile
         gainname = datasetId + '.GN'
-        logging.info('Searching for the gainfile {0} in {1}'.format(gainname, gainpath))
+        logger.info('Searching for the gainfile {0} in {1}'.format(gainname, gainpath))
         for path, dirs, files in os.walk(gainpath):
             for f in filter(lambda x: gainname in x, files):
                 gainfile.append(os.path.join(path, gainname))
@@ -47,18 +47,27 @@ def refine(miniSDMlist, workdir, preffileloc, gainpath = '/home/mchammer/evladat
         prefs = {'saveplots': True, 'savenoise': False, 'savesols': False, 'savecandcollection': True, 
                  'savecanddata': True, 'workdir': workdir, 'gainfile':gainfile[index], 
                  'sigma_image1': search_sigma, 'dmarr': list(np.linspace(dm-dm_frac*dm, dm+dm_frac*dm, dm_steps))}
+        if band=='l':
+                name = 'NRAOdefaultL'
+        elif band == 's':
+                name = 'NRAOdefaultS'
+        else:
+                logger.exception("Only L and S band supported")
+                return
+	
+        try:
+                st = state.State(sdmfile=sdm, sdmscan=1, inprefs=prefs, preffile = preffileloc, name=name)
+                cc = pipeline.pipeline_scan(st)
+        except AssertionError as e:
+                logger.exception('BDF not found for {0}, moving on to next sdm'.format(sdm)) 
 
-        st = state.State(sdmfile=sdm, sdmscan=1, inprefs=prefs, preffile = preffileloc, name='NRAOdefaultL')
-        cc = pipeline.pipeline_scan(st)
-
-    # # Classify and generate refinement plots
-    
+    # # Classify and generate refinement plots    
     # Classify the generated pickles using FETCH and generate refinement plots
     for miniSDM in miniSDMlist:
         sdmname = miniSDM.split('/')[-1]
         for pkl in glob.glob(st.prefs.workdir+'/'+'cands_*'+sdmname.split('/')[0]+'*.pkl'):
             if classify or refine:
-                logging.info('Refining and classifying pkl: {0}'.format(pkl))
+                logger.info('Refining and classifying pkl: {0}'.format(pkl))
                 ccs = list(candidates.iter_cands(pkl, select='candcollection'))
                 for cc in ccs:
                     cds = cc.canddata
@@ -67,16 +76,16 @@ def refine(miniSDMlist, workdir, preffileloc, gainpath = '/home/mchammer/evladat
                             if classify:
                                 payload = candidates.cd_to_fetch(cd, classify=True, save_png=True, show=True, mode = 'GPU'
                                                                  , outdir = workdir, devicenum='0')
-                                logging.info('FETCH FRB Probability of the candidate {0} is {1}'.format(cd.candid, payload))
+                                logger.info('FETCH FRB Probability of the candidate {0} is {1}'.format(cd.candid, payload))
                             if refine:
-                                logging.info('Generating Refinement plots')
+                                logger.info('Generating Refinement plots')
                                 cd_refine(cd, save=True, outdir = workdir)
                     else:
-                        logging.info('No candidate was found in cc: {0}'.format(cc))
+                        logger.info('No candidate was found in cc: {0}'.format(cc))
                         
                     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fine DM refinement of rfpipe candidates")
+    parser = argparse.ArgumentParser(description="Fine DM refinement of rfpipe candidates", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-sdm', '--sdm_path', help='Path of SDMs', required=True, type=str)
     parser.add_argument('-o', '--workdir', help='Working Directory', required=True, type=str)
     parser.add_argument('-g', '--gainpath', help='Path to gainfile directories', required=False, 
@@ -90,9 +99,10 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--search_sigma', help='Sigma of the image for search', required=False, type=float, default=7)
     parser.add_argument('-r', '--refinement_plots', help='Generate and Save refinement plots', action='store_true')
     parser.add_argument('-c', '--classify', help='Classify the candidates using FETCH', action='store_true')
-    parser.add_argument('-d', '--devicenum', help='Enter the GPU number to run pipeline', type=str, default=0, required=False)
+    parser.add_argument('-d', '--devicenum', help='Enter the GPU number to run pipeline', type=int, default=0, required=False)
+    parser.add_argument('-b', '--band', help='Enter the band of observation', type=str, default='l', required=False)
     args = parser.parse_args()
     
     refine(miniSDMlist=glob.glob(args.sdm_path), workdir=args.workdir, preffileloc=args.preffileloc, 
            gainpath=args.gainpath, search_sigma = args.search_sigma, refine = args.refinement_plots,
-           classify = args.classify, dm = args.DM, dm_frac=args.DM_fraction, dm_steps = args.DM_steps, devicenum=args.devicenum)
+           classify = args.classify, dm = args.DM, dm_frac=args.DM_fraction, dm_steps = args.DM_steps, devicenum=str(args.devicenum), band=args.band)
